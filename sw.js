41d284393e96656332a6832a84d8bac369332ae1
rahul19csu409-ui <rahul19csu409@gmail.com>
2026-08-20 22:45:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Shyam Bhajan Sangrah — service worker */
-const CACHE = "bhajan-shell-v16";
+const CACHE = "bhajan-shell-v17";
 const CORE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./pdfjs/pdf.min.js", "./pdfjs/pdf.worker.min.js"];
 
 self.addEventListener("install", (e) => {
